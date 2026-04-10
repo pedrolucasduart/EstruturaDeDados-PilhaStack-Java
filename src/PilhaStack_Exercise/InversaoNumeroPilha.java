7bb5c4d9b9c7ce153c806f14b4164c7d5a707acd
Pedro Lucas de Oliveira Duarte <pedrooliveiraa25@gmail.com>
2026-04-10 14:44:57 -0300

    Exercicio novo

--- src/PilhaStack_Exercise/InversaoNumeroPilha.java
+++ src/PilhaStack_Exercise/InversaoNumeroPilha.java
@@ -1,5 +1,44 @@
 package PilhaStack_Exercise;
 
 public class InversaoNumeroPilha {
+    public int topo;
+    public int capacidade;
+    public int[] elementos;
+
+
+    public InversaoNumeroPilha(int tamanho) {
+        topo = -1;
+        capacidade = tamanho;
+        elementos = new int[tamanho];
+    }
+
+    // Empilha um dígito no topo da pilha
+    public void push(int valor) {
+        if (estaCheia()) {
+            System.out.println("Overflow");
+            return;
+        }
+        topo++;
+        elementos[topo] = valor;
+    }
+
+    // Empilha cada dígito do número
+    public void empilharNormal() {
+        for (int i = 0; i < elementos.length; i++) {
+            System.out.print(elementos[i] + " ");
+        }
+    }
+
+    // Desempilha os dígitos formando o número invertido
+    public void empilharInvertido() {
+        for (int i = topo; i >= 0; i--) {
+            System.out.print(elementos[i] + " ");
+        }
+    }
+
+    // Verifica se a pilha está cheia
+    public boolean estaCheia() {
+        return topo == capacidade - 1;
+    }
 
 }
--- src/PilhaStack_Exercise/Main.java
+++ src/PilhaStack_Exercise/Main.java
@@ -1,4 +1,21 @@
 package PilhaStack_Exercise;
 
+import PilhaStack_Base.PilhaStack;
+
 public class Main {
+    public static void main(String[] args) {
+        InversaoNumeroPilha inversaoNumeroPilha = new InversaoNumeroPilha(4);
+
+        inversaoNumeroPilha.push(1);
+        inversaoNumeroPilha.push(2);
+        inversaoNumeroPilha.push(3);
+        inversaoNumeroPilha.push(4);
+
+        System.out.println("Entrada Normal: ");
+        inversaoNumeroPilha.empilharNormal();
+
+        System.out.println("\nNúmero invertido: ");
+        inversaoNumeroPilha.empilharInvertido();
+
+    }
 }
